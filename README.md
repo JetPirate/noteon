f3a23a871d48a45e0acfd27b38fa897b4fe97984
Oleh Fedorenko <fpostoleh@gmail.com>
2018-09-04 00:27:26 +0200

    1.0.0

---
+++ README.md
@@ -0,0 +1,45 @@
+JetPirate's NoteON
+====================
+
+Another cinnamon applet in this world
+---------------------
+
+![Main](https://i.imgur.com/RSAf3XX.png)
+
+which holds open your TODO list or file with notes you want to quick access.
+
+### Usage
+
+1. Modify content
+2. Press `SAVE`
+3. ?????
+4. PROFIT
+
+![Settings1](https://i.imgur.com/MQpRXo4.png)
+
+![Settings2](https://i.imgur.com/nCcnFWs.png)
+
+PS. Press `OPEN` to open the file with text editor you chose in settings.
+
+### Installation
+
+Simply copy the `noteon@jetpirate` folder from this repo to `~/.local/share/cinnamon/applets/`
+
+```shell
+$ git clone git@github.com:JetPirate/noteon.git
+$ cd noteon
+$ cp -r noteon@jetpirate/ ~/.local/share/cinnamon/applets/
+```
+
+Don't forget to enable the applet:
+
+`System Settings -> Applets -> NoteON`
+
+### Issues
+
+If you have an issue, please create it [here](
+  https://github.com/JetPirate/noteon/issues
+). Even though it's been "released", there are still known issues like:
+1. No possibility to simple `Ctrl-C/Ctrl-V`.
+
+It'll be fixed in the next release, so check for it via `Check for updates` button.
---
+++ noteon@jetpirate/applet.js
@@ -0,0 +1,136 @@
+const Applet = imports.ui.applet;
+const PopupMenu = imports.ui.popupMenu;
+const Settings = imports.ui.settings;
+const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
+const Main = imports.ui.main;
+const Lang = imports.lang;
+const AppRoot = imports.ui.appletManager.appletMeta['noteon@jetpirate'].path;
+const CurrentVersion = imports.ui.appletManager.appletMeta['noteon@jetpirate'].version;
+const Mainloop = imports.mainloop;
+const Tweener = imports.ui.tweener;
+const Soup = imports.gi.Soup;
+const _httpSession = new Soup.Session();
+
+function MyApplet(orientation, panel_height, instance_id) {
+  this._init(orientation, panel_height, instance_id);
+}
+
+MyApplet.prototype = {
+  __proto__: Applet.TextIconApplet.prototype,
+
+  _init: function(orientation, panel_height, instance_id) {
+    Applet.TextIconApplet.prototype._init.call(this, orientation, panel_height, instance_id);
+
+    this.menuManager = new PopupMenu.PopupMenuManager(this);
+    this.menu = new Applet.AppletPopupMenu(this, orientation);
+    this.menuManager.addMenu(this.menu);
+
+    this.settings = new Settings.AppletSettings(this, "noteon@jetpirate", instance_id);
+
+    this.settings.bind("icon-name", "icon_name", this.on_settings_changed);
+    this.settings.bind("use-custom-label",  "use_custom_label", this.on_settings_changed);
+    this.settings.bind("custom-label", "custom_label", this.on_settings_changed);
+    this.settings.bind("background-color", "background_color", this.on_settings_changed);
+    this.settings.bind("notification-color", "notification_color", this.on_settings_changed);
+    this.settings.bind("spinner-number", "spinner_number", this.on_settings_changed);
+    this.settings.bind("save-file", "save_file", this.on_settings_changed);
+    this.settings.bind("file-editor", "file_editor", this.on_settings_changed);
+
+    // Show help by default
+    this.save_file = "file///" + AppRoot + "/README";
+
+    this.save_file_content = new PopupMenu.PopupMenuItem("");
+    this.save_file_content.label.clutter_text.set_editable(true);
+    this.save_file_content.label.clutter_text.set_activatable(true);
+    this.save_file_content.label.clutter_text.set_reactive(true);
+    this.menu.addMenuItem(this.save_file_content);
+
+    this.command_open = new PopupMenu.PopupMenuItem("OPEN");
+    this.command_open.connect("activate", Lang.bind(this, this.command_open_callback));
+    this.menu.addMenuItem(this.command_open);
+
+    this.command_save = new PopupMenu.PopupMenuItem("SAVE");
+    this.command_save.connect("activate", Lang.bind(this, this.command_save_callback));
+    this.menu.addMenuItem(this.command_save);
+
+    this.on_settings_changed();
+  },
+  on_settings_changed: function() {
+    if (this.use_custom_label) {
+      this.set_applet_label(this.custom_label);
+    } else {
+      this.set_applet_label(_("~hello there~"));
+    }
+
+    let icon_file = Gio.File.new_for_path(this.icon_name);
+    if (icon_file.query_exists(null)) {
+      this.set_applet_icon_path(this.icon_name);
+    } else {
+      this.set_applet_icon_name(this.icon_name);
+    }
+
+    this.update_save_file();
+    this.actor.style = "background-color:" + this.background_color + "; width:" + this.spinner_number + "px";
+  },
+  on_applet_clicked: function(event) {
+    this.update_save_file();
+    this.menu.toggle();
+  },
+  on_applet_removed_from_panel: function() {
+    this.settings.finalize();
+  },
+  update_save_file: function() {
+    if (GLib.file_test(this.get_save_file_path(), GLib.FileTest.EXISTS)) {
+      this.save_file_content.label.clutter_text.set_text(GLib.file_get_contents(this.get_save_file_path())[1].toString());
+    } else {
+      this.save_file_content.label.clutter_text.set_text("Choose a file in applet settings.");
+    }
+  },
+  get_save_file_path: function() {
+    return this.save_file.substring(6);
+  },
+  command_open_callback: function() {
+    Main.Util.spawnCommandLine(this.file_editor + " " + this.get_save_file_path());
+  },
+  command_save_callback: function() {
+    GLib.file_set_contents(this.get_save_file_path(), this.save_file_content.label.clutter_text.get_text());
+  },
+  check_updates_callback: function() {
+    var answer = "";
+    var message = Soup.Message.new('GET', 'https://api.github.com/repos/JetPirate/noteon/releases');
+    _httpSession.user_agent = 'cinnamon';
+    _httpSession.send_message(message);
+    response = JSON.parse(message.response_body.data);
+    last_version = response[0]["tag_name"];
+    if (last_version > CurrentVersion) {
+      answer = " ~new version (" + last_version + ") is available!~";
+    } else {
+      answer = " ~already latest!~";
+    }
+    this.actor.style = "background-color:" + this.notification_color + "; width:" + (answer.length * 10) + "px";
+    this.set_applet_label(answer);
+
+    var timeoutId = Mainloop.timeout_add(3000, Lang.bind(this, function() {
+      this.on_settings_changed();
+    }));
+
+    Tweener.addTween(this._applet_icon, {
+      margin_left: 10,
+      time: 0.5,
+      transition: "easeInSine",
+      onComplete: function() {
+        Tweener.addTween(this._applet_icon, {
+          margin_left: 0,
+          time: 0.5,
+          transition: "easeInSine"
+        });
+      },
+      onCompleteScope: this
+    });
+  }
+};
+
+function main(metadata, orientation, panel_height, instance_id) {
+  return new MyApplet(orientation, panel_height, instance_id);
+}
